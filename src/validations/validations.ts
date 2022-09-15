8afc1bd1ac5d13546da8542c772129f78f6e75d5
Slava <slava91petrushin@yandex.ru>
2022-09-16 01:25:23 +0300

    check

--- src/validations/validations.ts
+++ src/validations/validations.ts
@@ -6,7 +6,7 @@ let Resolutions = [ "P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P
 export const validateCreateVideo = [
 	body('author', {field: "author", message: "Укажите автора"}).isLength({max: 20}).isString(),
 	body('title', {field: "title", message: "Укажите заголовок"}).isLength({max: 40}).isString(),
-	body('availableResolutions', {field: "title", message: "Укажите заголовок"}).custom((element) => element.every((el: any ) => Resolutions.includes(el)))
+	body('availableResolutions', {field: "availableResolutions", message: "Укажите разрешение"}).custom((element) => element.every((el: any ) => Resolutions.includes(el)))
 ];
 
 export const validateUpdateVideo = [
